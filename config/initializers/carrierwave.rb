e69e83efa5b1d06ff75cee121197aa7312e7a184
sugita-seiya <satreu16@ezweb.ne.jp>
2020-02-03 17:56:46 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,3 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'freemarketsample67i'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample67i'
 end
-
-
-# aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-# aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
