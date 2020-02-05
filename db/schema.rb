6adc2b9061c2d0acc17663c037c67f73e6ecc32c
Utronic55 <halfboiled5@gmail.com>
2020-02-05 18:22:41 +0900

    fix.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_041051) do
+ActiveRecord::Schema.define(version: 2020_02_04_071955) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.string "name"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "brand_id", null: false
@@ -29,14 +37,5 @@ ActiveRecord::Schema.define(version: 2020_02_04_041051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-end
 
-ActiveRecord::Schema.define(version: 2020_02_04_063617) do
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
 end
