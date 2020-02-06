472e29e37d5645079d1cf96b32da1363a2f36e5d
Utronic55 <halfboiled5@gmail.com>
2020-02-06 17:09:44 +0900

    １

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,18 @@
 class ItemsController < ApplicationController
-
+  def index
+    @items = Item.includes(:user).order('created_at DESC').limit(9)
+  end
   def new
     @item = Item.new
   end
   def show
     @item = Item.find(params[:id])
   end
+
+  private
+
+  def product_params
+    params.require(:item).permit(:name, :text, :item_image_id, :buyer_id, :seler_id, :category_id, :brand_id, :delivery_method, :size, :delivery_charge, :delivery_date, :price, :quality, :area).merge(user_id: current_user.id)
+  end
 end
+
