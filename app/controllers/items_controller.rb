a2f71a5785d6d24a712336640a8293f1530f741e
Utronic55 <halfboiled5@gmail.com>
2020-02-12 19:40:35 +0900

    fix6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,13 @@ class ItemsController < ApplicationController
     @item_images = @item.item_images.build
     @category_parent_array = Category.where(ancestry: nil)
     # itemに紐づいていいる孫カテゴリーの親である子カテゴリが属している子カテゴリーの一覧を配列で取得
-    # @category_child_array = @item.category.parent.parent.children
+    @category_child_array = @item.category.parent.parent.children
     # # itemに紐づいていいる孫カテゴリーが属している孫カテゴリーの一覧を配列で取得
-    # @category_grandchild_array = @item.category.parent.children
+    @category_grandchild_array = @item.category.parent.children
     
   end
   
   def create
-    binding.pry
     @item = Item.new(item_params)
     @item.save!
     redirect_to root_path
