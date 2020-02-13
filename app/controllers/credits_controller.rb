082386792ca655c30c04a26983592d85ee480996
tukki-r <57993375+tukki-r@users.noreply.github.com>
2020-02-13 22:41:17 +0900

    詳細確認、削除機能

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,10 +1,11 @@
 class CreditsController < ApplicationController
+
+  before_action :get_payjp_info, only: [:new_create, :create, :delete, :show]
   
   def new
   end
 
   def create
-    console.log
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
@@ -14,7 +15,7 @@ class CreditsController < ApplicationController
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to root_path
+        redirect_to action: "show"
       else
         redirect_to action: "edit", id: current_user.id
       end
@@ -22,8 +23,29 @@ class CreditsController < ApplicationController
   end
 
   def confirmation
-    # card = current_user.credit_cards
-    # redirect_to action: "show" if card.exists?
+  end
+
+  def show #Cardのデータpayjpに送り情報を取り出します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  def delete #PayjpとCardデータベースを削除します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_071422) do
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
 ActiveRecord::Schema.define(version: 2020_02_12_060921) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -31,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_02_12_060921) do
     t.datetime "updated_at", null: false
     t.string "phone_number"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,11 +65,6 @@ ActiveRecord::Schema.define(version: 2020_02_12_060921) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "signups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,7 +81,6 @@ ActiveRecord::Schema.define(version: 2020_02_12_060921) do
     t.integer "birth_day", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_confirmation"
     t.string "password"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
