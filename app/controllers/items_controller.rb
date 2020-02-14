baf38d80b04d117e88d79445995dc078fee5bd20
Utronic55 <halfboiled5@gmail.com>
2020-02-14 13:26:53 +0900

    finish

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,24 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item_images = @item.item_images.build
-    @category_parent_array = Category.where(ancestry: nil)
-    # itemに紐づいていいる孫カテゴリーの親である子カテゴリが属している子カテゴリーの一覧を配列で取得
-    @category_child_array = @item.category.parent.parent.children
-    # # itemに紐づいていいる孫カテゴリーが属している孫カテゴリーの一覧を配列で取得
-    @category_grandchild_array = @item.category.parent.children
+    @category_parent_array = ["---"]
+    #データベースから、親カテゴリーのみ抽出し、配列化
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+  end
+
+  # 以下全て、formatはjsonのみ
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
     
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   root to: "top#index"
-  resources :items, only: [:new, :show,:destroy]
+  resources :items, only: [:index,:new, :show,:edit,:destroy] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
   :sessions => 'users/sessions'
