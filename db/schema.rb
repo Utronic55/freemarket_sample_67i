8e2aa5f88ddf3b7d46b879df87153f72dee83893
tukki-r <57993375+tukki-r@users.noreply.github.com>
2020-02-17 18:44:48 +0900

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_044348) do
+ActiveRecord::Schema.define(version: 2020_02_17_093838) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "number", null: false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2020_02_15_044348) do
   create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "customer_id"
+    t.string "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -51,8 +59,6 @@ ActiveRecord::Schema.define(version: 2020_02_15_044348) do
     t.integer "category_id"
     t.integer "child_category_id"
     t.integer "grandchild_category_id"
-    t.integer "buyer_id", null: false
-    t.integer "saler_id", null: false
     t.string "name", null: false
     t.integer "price", null: false
     t.text "text", limit: 4294967295, null: false
