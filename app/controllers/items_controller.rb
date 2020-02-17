66c29fdf4386cd63f9018341fcd5167051ab8a1f
Utronic55 <halfboiled5@gmail.com>
2020-02-17 18:18:27 +0900

    fix1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,17 +34,12 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    # @user = User.find(params[:id])
   end
 
   
 end
 
 private
-
-# def product_params
-#   params.require(:item).permit(:name, :text, :item_image_id, :buyer_id, :seler_id, :category_id, :brand_id, :delivery_method, :size, :delivery_charge, :delivery_date, :price, :quality, :area).merge(user_id: current_user.id)
-# end
 
 def item_params
   params.require(:item).permit(:name,:text,:category_id,:child_category_id,:grandchild_category_id,:quality,:delivery_charge,:area_id,:delivery_date,:price,item_images_attributes: [:image]).merge(saler_id: current_user.id)
