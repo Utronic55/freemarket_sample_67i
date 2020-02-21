76dbffcc5a1d0b6094bdd21b09eef9c0951f11cf
Utronic55 <halfboiled5@gmail.com>
2020-02-21 16:39:44 +0900

    ok

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    if @item.save
+    if @item.save!
       redirect_to root_path
     else
       redirect_to root_path,notice: '出品に失敗しました'
