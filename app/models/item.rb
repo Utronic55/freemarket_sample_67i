cb66871556615395c2b0911312151d12867a4d5b
Utronic55 <halfboiled5@gmail.com>
2020-02-21 20:27:31 +0900

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,9 @@ class Item < ApplicationRecord
   has_many :item_images
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User",optional: true
-  belongs_to :category
+  belongs_to :category,class_name: "Category"
+  belongs_to :child_category,class_name: "Category"
+  belongs_to :grandchild_category,class_name: "Category"
   accepts_nested_attributes_for :item_images
   validates :item_images , presence: true
 
